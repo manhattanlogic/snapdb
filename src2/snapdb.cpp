e473d331b0ce2a530aaa61019fbdf2cc44f3c204
manhattanlogic <roman@manhattanlogic.com>
2017-04-21 13:13:24 -0400

    x

--- src2/snapdb.cpp
+++ src2/snapdb.cpp
@@ -129,40 +129,8 @@ std::unordered_map<unsigned long, single_user_history *> histories[THREADS];
 
 std::unordered_map<unsigned long, single_json_history * > json_history;
 
-struct test_data {
-  unsigned long vid;
-  unsigned long ts;
-};
 
-/*
-test_data test_json(char * line) {
-  test_data result = {};
-  rapidjson::Document d;
-  char * tab = strchr(line, '\t');
-  if (tab == NULL) return result;
-  tab = strchr((tab + 1), '\t');
-  if (tab == NULL) return result;
-  std::string json = (tab+1);
-  json = replace_all(json, "\\'","'");
-  json = replace_all(json, "\\\\","\\");
-  d.Parse(json.c_str());
-  try {
-    result.vid = d["vid"].GetUint64();
-  } catch (...) {
-    result.vid = 0;
-  }
-  try {
-    struct tm tm;
-    auto str_ts = d["events"][0]["ts"].GetString();
-    strptime(str_ts, "%Y-%d-%mT%H:%M:%S", &tm);
-    result.ts = mktime(&tm);
-  } catch (...) {
-    result.ts = 0;
-  }
-  return result;
-}
-*/
-  
+
 rapidjson::Document * parse_json(char * line) {
   rapidjson::Document * d = new rapidjson::Document();
   
@@ -179,8 +147,6 @@ rapidjson::Document * parse_json(char * line) {
     std::cerr << "json error\n" << json << "\n";
     return d;
   }
-
-  return d;
   
   for (int i = 0; i < (*d)["events"].Size(); i++) {
     if ((*d)["events"][i]["subids"].HasMember("ensighten") &&
@@ -192,25 +158,6 @@ rapidjson::Document * parse_json(char * line) {
     }
   }
   return d;
-}
-
-test_data test_json(char * line) {
-  auto json = parse_json(line);
-  test_data result = {};
-  try {
-    result.vid = (*json)["vid"].GetUint64();
-  } catch (...) {
-  }
-  try {
-    struct tm tm;
-    auto str_ts = (*json)["events"][0]["ts"].GetString();
-    strptime(str_ts, "%Y-%d-%mT%H:%M:%S", &tm);
-    result.ts = mktime(&tm);
-  } catch (...) {
-    result.ts = 0;
-  }
-  delete json;
-  return result;
 }
 
 
@@ -245,7 +192,7 @@ int current_action = 0; // compute_converters
 
 
 
-void process_result(test_data data, unsigned long file_position) {
+void process_result(rapidjson::Document * data, unsigned long file_position) {
   //std::lock_guard<std::mutex> guard(result_processor_mutex);
 
   
@@ -253,7 +200,7 @@ void process_result(test_data data, unsigned long file_position) {
   
   result_processor_mutex.lock();
   
-  /*
+  
   unsigned long vid = 0;
   unsigned long ts = 0;
   
@@ -262,8 +209,6 @@ void process_result(test_data data, unsigned long file_position) {
   } catch (...) {
   }
 
-  
-  
   try {
     struct tm tm;
     auto str_ts = (*data)["events"][0]["ts"].GetString();
@@ -271,11 +216,7 @@ void process_result(test_data data, unsigned long file_position) {
     ts = mktime(&tm);
   } catch (...) {
   }
-  */
-  
-  unsigned long vid = data.vid;
-  unsigned long ts = ts;
-  
+
 
   counter += 1;
   if (counter % 10000 == 0) {
@@ -292,10 +233,10 @@ void process_result(test_data data, unsigned long file_position) {
   
   
   json_history_entry * je = new json_history_entry;
-  //je->document = data;
   je->file_position = file_position;
   it->second->history[ts] = je;
 
+  delete data;
   result_processor_mutex.unlock();
 }
 
@@ -304,9 +245,7 @@ void thread_runner(int id) {
   char * line = (char *) malloc(1024 * 1024 * 64); // 64 MB to be safe
   long file_position;
   while ((file_position = get_next_line(line)) >= 0) {
-    //auto result = parse_json(line);
-    auto result = test_json(line);
-    
+    auto result = parse_json(line);
     process_result(result, file_position);
   }
 }
