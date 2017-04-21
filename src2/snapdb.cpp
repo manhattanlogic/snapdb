3c3ef321cd39b09196968d1450df33cab7825890
manhattanlogic <roman@manhattanlogic.com>
2017-04-21 11:26:38 -0400

    second test

--- src2/snapdb.cpp
+++ src2/snapdb.cpp
@@ -196,8 +196,6 @@ void process_result(rapidjson::Document * data, unsigned long file_position) {
   if (counter % 10000 == 0) {
     std::cerr << counter <<  "\n";
   }
-
-  return;
   
   unsigned long vid = 0;
   unsigned long ts = 0;
