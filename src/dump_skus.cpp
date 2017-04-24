3cd239b107563d16b5809a2636c4d8bee3db9614
illegalplumbing <illegalplumbing@gmail.com>
2017-04-24 17:18:29 -0400

    x

---
+++ src/dump_skus.cpp
@@ -0,0 +1,52 @@
+#include <string>
+#include <stdlib.h>
+#include <stdio.h>
+#include "snapdb.hpp"
+#include <sstream>
+#include <unordered_set>
+#include <memory.h>
+
+
+
+
+extern "C"
+char * query() {
+  std::stringstream result;
+  
+  for (auto i = json_history.begin(); i != json_history.end(); i++) {
+    std::vector <std::string> skus;
+    auto start =  i->second->history.begin()->second.ts;
+    auto end =  i->second->history.rbegin()->second.ts;
+    if (end - start < 1000) continue;
+     
+    for (auto j = i->second->history.begin(); j != i->second->history.end(); j++) {
+      if (j->second.events.size() != 1) continue;
+      for (int e = 0; e < j->second.events.size(); e++) {
+	auto event = j->second.events[e];
+	if (event.ensighten.exists) {
+	  bool is_product = event.ensighten.pageType == "PRODUCT";
+	  for (int it = 0; it < event.ensighten.items.size(); it ++) {
+	    if ((is_product) || (event.ensighten.items[it].tag == "productpage")) {
+		skus.push_back(event.ensighten.items[it].sku);
+	      }
+	  } // items
+	} // ensighten
+      } // enents
+    } // history
+  
+
+    if (skus.size() < 3) continue;
+    for (int j = 0; j < skus.size(); j++) {
+      if (j > 0) result << " ";
+      result << skus[j];
+    }
+    result << "\n";
+  } // user
+  
+  // end of custom code
+  char * buffer = (char *)malloc(result.str().size() + 1);
+  memcpy(buffer, result.str().c_str(), result.str().size());
+  buffer[result.str().size()] = 0;
+  return buffer;
+}
+
