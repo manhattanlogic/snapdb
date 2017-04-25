42667be51f321779043b5aefa678d02340b9896f
illegalplumbing <illegalplumbing@gmail.com>
2017-04-25 00:32:11 -0400

    x

--- src/compute_user_vectors.cpp
+++ src/compute_user_vectors.cpp
@@ -44,66 +44,69 @@ char * query() {
   std::cerr << "query started\n";
   std::cout << "query started\n";
   result << "roma durak\n";
-  load_word2vec("sku_vectors.csv");
-  
-  
-  std::vector<float> user_value;
-  user_value.resize(w2v_size);
 
-  result << w2v.size() << "vectors loaded\n";
-  std::cerr << w2v.size() << "vectors loaded\n";
-  std::cout << w2v.size() << "vectors loaded\n";
-  int limit = 1000;
+  if (false) {
   
-  for (auto i = json_history.begin(); i != json_history.end(); i++) {
-    if (limit == 0) break;
-    limit --;
-    std::fill(user_value.begin(), user_value.end(), 0.0);
-    std::vector <std::string> skus;
-    int n = 0;
+    load_word2vec("sku_vectors.csv");
+  
+  
+    std::vector<float> user_value;
+    user_value.resize(w2v_size);
+
+    result << w2v.size() << "vectors loaded\n";
+    std::cerr << w2v.size() << "vectors loaded\n";
+    std::cout << w2v.size() << "vectors loaded\n";
+    int limit = 1000;
+  
+    for (auto i = json_history.begin(); i != json_history.end(); i++) {
+      if (limit == 0) break;
+      limit --;
+      std::fill(user_value.begin(), user_value.end(), 0.0);
+      std::vector <std::string> skus;
+      int n = 0;
     
-    auto start =  i->second->history.begin()->second.ts;
-    auto end =  i->second->history.rbegin()->second.ts;
-    if (end - start < 1000) continue;
-    bool is_converter = false;
-    for (auto j = i->second->history.begin(); j != i->second->history.end(); j++) {
-      if (j->second.events.size() != 1) continue;
-      for (int e = 0; e < j->second.events.size(); e++) {
-	auto event = j->second.events[e];
-	result << event.ensighten.exists << "\n";
-	if (event.ensighten.exists) {
-	  bool is_product = event.ensighten.pageType == "PRODUCT";
-	  result << event.ensighten.items.size() << "\n";
-	  for (int it = 0; it < event.ensighten.items.size(); it ++) {
-	    if ((is_product) || (event.ensighten.items[it].tag == "productpage")) {
-	      skus.push_back(event.ensighten.items[it].sku);
-	      auto sku_vector = w2v.find(event.ensighten.items[it].sku);
-	      result << event.ensighten.items[it].sku << "\n";
-	      if (sku_vector != w2v.end()) {
-		result << "ok\n";
-		for (int z = 0; z < w2v_size; z++) {
-		  user_value[z] += sku_vector->second[z];
+      auto start =  i->second->history.begin()->second.ts;
+      auto end =  i->second->history.rbegin()->second.ts;
+      if (end - start < 1000) continue;
+      bool is_converter = false;
+      for (auto j = i->second->history.begin(); j != i->second->history.end(); j++) {
+	if (j->second.events.size() != 1) continue;
+	for (int e = 0; e < j->second.events.size(); e++) {
+	  auto event = j->second.events[e];
+	  result << event.ensighten.exists << "\n";
+	  if (event.ensighten.exists) {
+	    bool is_product = event.ensighten.pageType == "PRODUCT";
+	    result << event.ensighten.items.size() << "\n";
+	    for (int it = 0; it < event.ensighten.items.size(); it ++) {
+	      if ((is_product) || (event.ensighten.items[it].tag == "productpage")) {
+		skus.push_back(event.ensighten.items[it].sku);
+		auto sku_vector = w2v.find(event.ensighten.items[it].sku);
+		result << event.ensighten.items[it].sku << "\n";
+		if (sku_vector != w2v.end()) {
+		  result << "ok\n";
+		  for (int z = 0; z < w2v_size; z++) {
+		    user_value[z] += sku_vector->second[z];
+		  }
+		  n++;
 		}
-		n++;
 	      }
-	    }
-	    if ((is_product) || (event.ensighten.items[it].tag == "order")) {
-	      is_converter = true;
-	    }
-	  } // items
-	} // ensighten
-      } // enents
-    } // history
-    if (n > 0) {
-      result << i->first << "\t" << is_converter;
-      for (int z = 0; z < w2v_size; z++) {
-	result << "\t" << user_value[z] / n;
+	      if ((is_product) || (event.ensighten.items[it].tag == "order")) {
+		is_converter = true;
+	      }
+	    } // items
+	  } // ensighten
+	} // enents
+      } // history
+      if (n > 0) {
+	result << i->first << "\t" << is_converter;
+	for (int z = 0; z < w2v_size; z++) {
+	  result << "\t" << user_value[z] / n;
+	}
+	result << "\n";
       }
-      result << "\n";
-    }
 
-  } // user
-  
+    } // user
+  }
   // end of custom code
   char * buffer = (char *)malloc(result.str().size() + 1);
   memcpy(buffer, result.str().c_str(), result.str().size());
@@ -114,12 +117,12 @@ char * query() {
 
 
 /*
-int main(int argc, char ** argv) {
+  int main(int argc, char ** argv) {
   load_word2vec("sku_vectors.csv");
   for (int i = 0; i < w2v.begin()->second.size(); i++) {
-    std::cerr << w2v.begin()->second[i] << " ";
+  std::cerr << w2v.begin()->second[i] << " ";
   }
   std::cerr << "\n";
   return 0;
-}
+  }
 */
