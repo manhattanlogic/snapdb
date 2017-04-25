d238833ad9f54112e15fd45727207152a6cae408
illegalplumbing <illegalplumbing@gmail.com>
2017-04-24 23:17:50 -0400

    x

--- src/compute_user_vectors.cpp
+++ src/compute_user_vectors.cpp
@@ -40,6 +40,7 @@ void load_word2vec(std::string filename) {
 
 extern "C"
 char * query() {
+  load_word2vec();
   std::stringstream result;
   std::vector<float> user_value;
   user_value.resize(w2v_size);
@@ -47,8 +48,8 @@ char * query() {
   int limit = 1000;
   
   for (auto i = json_history.begin(); i != json_history.end(); i++) {
-    if (limit == 0) break;
-    limit --;
+    //if (limit == 0) break;
+    //limit --;
     std::fill(user_value.begin(), user_value.end(), 0.0);
     std::vector <std::string> skus;
     int n = 0;
@@ -67,7 +68,7 @@ char * query() {
 	    if ((is_product) || (event.ensighten.items[it].tag == "productpage")) {
 	      skus.push_back(event.ensighten.items[it].sku);
 	      auto sku_vector = w2v.find(event.ensighten.items[it].sku);
-	      result << event.ensighten.items[it].sku << "\n";
+	      //result << event.ensighten.items[it].sku << "\n";
 	      if (sku_vector != w2v.end()) {
 		for (int z = 0; z < w2v_size; z++) {
 		  user_value[z] += sku_vector->second[z];
