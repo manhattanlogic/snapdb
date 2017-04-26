47cb2cae77a9f0038ef4ed2f6f61807d44197255
illegalplumbing <illegalplumbing@gmail.com>
2017-04-26 18:54:04 -0400

    x

--- tsne/DEA.py
+++ tsne/DEA.py
@@ -136,7 +136,7 @@ if __name__ == "__main__":
         np.save(open("data.np","wb"), data)
         print ("csv data loaded. numpy data saved")
 
-    dea = DEA(p_epochs=20, t_epochs=100)
+    dea = DEA(p_epochs=20, t_epochs=10)
     dea.load_weights("weights.pkl")
 
     if len(sys.argv) > 1 and sys.argv[1] == "skip":
@@ -145,7 +145,13 @@ if __name__ == "__main__":
         dea.train(data[:,2:], pretrain=True)
 
     dea.save_weights("weights.pkl")
+
+    converters = np.where(data[:,1]==1)[0]
+    non_converters = np.where(data[:,1]==0)[0]
+
+    projection = dea.get_projection(data[non_converters,2:])
+    plt.scatter(projection[:,0],projection[:,1], s=0.1, color="black")
+    projection = dea.get_projection(data[converters,2:])
+    plt.scatter(projection[:,0],projection[:,1], s=0.1, color="red")
     
-    projection = dea.get_projection(data[:,2:])
-    plt.scatter(projection[:,0],projection[:,1], s=1, c=data[:,1])
     plt.savefig('graph.png')
