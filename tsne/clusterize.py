d09112a055cbbb48dd0c436f4be77e1afcc64359
manhattanlogic <roman@manhattanlogic.com>
2017-04-29 21:23:31 -0400

    x

--- tsne/clusterize.py
+++ tsne/clusterize.py
@@ -31,6 +31,7 @@ def draw_circle(_x, _y, data, diameter, color=[255,255,255]):
 
 class VARIATOR:
     def __init__(self, input_size=2, num_clusters = 16, hidden_size = 8):
+        self.weight_noise_sigma = 0.1
         self.num_clusters = num_clusters
         self.input_size = input_size
         self.input = tf.placeholder(tf.float32, [None, input_size])
@@ -53,9 +54,20 @@ class VARIATOR:
         self.optimizer = tf.train.AdamOptimizer(0.01)
         self.train_op = (self.optimizer.minimize(tf.reduce_mean(variances)), tf.reduce_mean(variances))
 
+        self.weight_noiser = []
+        for i in range(0, len(self.weights)):
+            self.weight_noiser.append((
+            self.weights[i][0].assign(self.weights[i][0] + tf.random_normal(tf.shape(self.weights[i][0]),
+                                                                                stddev=self.weight_noise_sigma)),
+            self.weights[i][1].assign(self.weights[i][1] + tf.random_normal(tf.shape(self.weights[i][1]),
+                                                                                stddev=self.weight_noise_sigma))
+                                                                            ))
+
+
     def save_weights(self, filename):
         weights = self.sess.run(self.weights)
         pickle.dump(weights, open(filename,"wb"))
+        
         
 if __name__ == "__main__":
 
@@ -86,6 +98,7 @@ if __name__ == "__main__":
 
 
     for i in range(0, 1000000):
+        dea.sess.run(dea.weight_noiser)
         _, error = dea.sess.run(variator.train_op, feed_dict = {variator.input: projection})
         clusters = np.argmax(dea.sess.run(variator.output, feed_dict = {variator.input: projection}), axis=1)
         c_colors = np.array(colors)[clusters]
