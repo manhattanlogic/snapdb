cc7046d0a907b8dcc4dc81c03ed9f88450a54c22
manhattanlogic <roman@manhattanlogic.com>
2017-05-08 20:55:59 -0400

    a

--- tsne/VAE.py
+++ tsne/VAE.py
@@ -122,7 +122,7 @@ class DEA:
             self.vae_loss = self.__get_vae_cost(mean, stddev)
             
             self.ae["error"] = tf.reduce_mean(dot / (n1 * n2))
-            self.ae["learn"] = (self.optimizer.minimize([-self.ae["error"], self.vae_loss]), self.ae["error"], self.vae_loss)
+            self.ae["learn"] = (self.optimizer.minimize((-self.ae["error"], self.vae_loss)), self.ae["error"], self.vae_loss)
             self.ae["projection"] = self.ae["layers"][len(layer_shapes)-1]
             self.ae["preprojection"] = self.ae["layers"][len(layer_shapes)-2]
             self.ae["postprojection"] = self.ae["layers"][len(layer_shapes)]
