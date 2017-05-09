f48d3ad29e478560e68a316667aa17306836e053
manhattanlogic <roman@manhattanlogic.com>
2017-05-09 19:15:42 -0400

    x

--- VAE/main.py
+++ VAE/main.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     sess.run(tf.global_variables_initializer())
     writer = tf.summary.FileWriter('logs', sess.graph)
     
-    batch_size = 1024 * 16
+    batch_size = 1024 * 16 * 4
     shuffler = np.arange(0, data.shape[0])
     
 
