e4eadfb69488a2914c5fe4bf52c66d0c89c61445
manhattanlogic <roman@manhattanlogic.com>
2017-05-09 18:18:20 -0400

    x

--- VAE/model.py
+++ VAE/model.py
@@ -56,6 +56,6 @@ class Decoder:
         
         self.loss = tf.reduce_mean(tf.square(output - self.target))
         # encoder.vae_loss
-        self.learn = (self.optimizer.minimize(self.loss + encoder.vae_loss),
+        self.learn = (self.optimizer.minimize(self.loss),
                           self.loss, encoder.vae_loss)
         
