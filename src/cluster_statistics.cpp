cb683ae91084251ace80f4c0e61ac22c45846abe
manhattanlogic <roman@manhattanlogic.com>
2017-05-11 16:09:33 -0400

    a

--- src/cluster_statistics.cpp
+++ src/cluster_statistics.cpp
@@ -21,29 +21,55 @@ void load_cluster_data(std::string filename) {
     clusters[vid] = cluster;
   }
 }
+
+
+struct u_stats {
+  long users = 0;
+  long converters = 0;
+};
+
 extern "C"
 char * query() {
   load_cluster_data("../tsne/clusters.csv");
   std::cerr << clusters.size() << " clustered users loaded\n";
 
-  std::map<int, long> cluster_totals;
+  std::map<int, u_stats> cluster_totals;
 
   for (auto c = clusters.begin(); c != clusters.end(); c++) {
     auto it = json_history.find(c->first);
     if (it == json_history.end()) continue;
+
+    bool converter = false;
     
+    for (auto h = it->second->history.begin(); h != it->second->history.end(); h++) {
+      if (h->second.events == NULL) continue;
+      for (auto e = h->second.events->begin(); e != h->second.events->end(); e++) {
+	if (!(e->ensighten.exists)) continue;
+	
+	for (auto i = e->ensighten.items.begin(); i != e->ensighten.items.end(); i++) {
+	  if (i->tag == "order") {
+	    converter = true;
+	  }
+	}
+	
+      }
+    }
+
     auto it2 = cluster_totals.find(c->second);
     if (it2 == cluster_totals.end()) {
-      cluster_totals[c->second] = 1;
-    } else {
-      it2->second++;
+      cluster_totals[c->second] = {};
+      it2 = cluster_totals.find(c->second);
+    } 
+    it2->second.users++;
+    if (converter) {
+      it2->second.converters++;
     }
   }
 
   std::stringstream str_result;
 
   for (auto it3 = cluster_totals.begin(); it3 != cluster_totals.end(); it3++) {
-    str_result << it3->first << "\t" << it3->second << "\n";
+    str_result << it3->first << "\t" << it3->second.users << "\t" << it3->second.converters << "\n";
   }
 
 
