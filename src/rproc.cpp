0209d73fe50ae0f2797a004fb216fdc72ffe419d
manhattanlogic <roman@manhattanlogic.com>
2017-05-12 19:15:56 -0400

    a

--- src/rproc.cpp
+++ src/rproc.cpp
@@ -7,105 +7,56 @@
 #include <unordered_set>
 #include <unordered_map>
 #include <memory.h>
+#include <fstream>
 
 extern std::string test_string;
 
+std::vector<std::unordered_map<std::string, long> > crumb_stats;
 
 extern "C"
 char * query() {
-  auto test = load_json_at_position(0);
+  std::ofstream file("sku_stats.csv");
   std::unordered_map<int, int> vector_stats;
   std::stringstream result;
   for (auto i = json_history.begin(); i != json_history.end(); i++) {
     for (auto j = i->second->history.begin(); j != i->second->history.end(); j++) {
-      auto it = vector_stats.find(j->second.events->size());
-      if (it == vector_stats.end()) {
-	vector_stats[j->second.events->size()] = 1;
-      } else {
-	it->second++;
-      }
-    }
-  }
-
-  for (auto i = vector_stats.begin(); i != vector_stats.end(); i++) {
-    result << i->first << "," << i->second << "\n";
-  }
-
-
-  /*
-  std::stringstream result;
-  history_filter.clear();
-
-  std::vector<int> stats[4] = {{0,0},{0,0},{0,0},{0,0}};
-  
-  for (auto i = json_history.begin(); i != json_history.end(); i++) {
-    if (i->second->history.size() < 2) continue;
-
-    auto first = i->second->history.begin();
-    auto last = i->second->history.rbegin();
-
-    if ((last->second.ts - first->second.ts) < 1000) continue;
-    
-    bool is_valid = false;
-    bool is_converter = false;
-    // all single(1), all plural(2), some single-some plural(3)
-    int plural  = 0;
-    for (auto j = i->second->history.begin(); j != i->second->history.end(); j++) {
-      if ((j->second.events == NULL) || (j->second.events->size() <= 0)) continue;
-      bool comma = j->second.pixels.find_first_of(",") != std::string::npos;
-      switch (plural) {
-      case 0:
-	if (comma) {
-	  plural = 2;
-	} else {
-	  plural = 1; 
+      bool is_product = false;
+      if (j->second.events == NULL) continue;
+      for (auto e = j->second.events->begin(); e != j->second.events->end(); e++) {
+	if (!(e->ensighten.exists)) continue;
+	for (auto ii = e->ensighten.items.begin(); ii != e->ensighten.items.end(); ii++) {
+	  if ((e -> ensighten.pageType == "PRODUCT") || (ii -> tag == "productpage")) {
+	    is_product = true;
+	    break;
+	  }
 	}
-	break;
-      case 1:
-	if (comma) plural = 3;
-	break;
-      case 2:
-	if (!(comma)) plural = 3;
-	break;
-      }
-      for (int e = 0; e < j->second.events->size(); e++) {
-	auto event = (*(j->second.events))[e];
-	if (!(event.ensighten.exists)) continue;
-
-	if ((event.ensighten.camGroup == "DglBrand") ||
-	  (event.ensighten.camSource == "Digital Brand") ||
-	    (event.ensighten.camSource == "RevJet Acq")) {
-	  is_valid = true;
-	}
-	
-	for (int k = 0; k < event.ensighten.items.size(); k++) {
-	  if (event.ensighten.items[k].tag == "order") {
-	    is_converter = true;
+	if (is_product) {
+	  for (int q = 0; q < e->ensighten.crumbs.size(); q++) {
+	    if (q >= crumb_stats.size()) {
+	      std::unordered_map<std::string, long> map;
+	      crumb_stats.push_back(map);
+	    }
+	    auto it = crumb_stats[q].find(e->ensighten.crumbs[q]);
+	    if (it == crumb_stats[q].end()) {
+	      crumb_stats[q][e->ensighten.crumbs[q]] = 1;
+	    } else {
+	      it->second++;
+	    }
 	  }
 	}
       }
     }
-    if (is_valid)  {
-      history_filter.insert(i->first);
-    }
-
-    if (true) {
-      stats[plural][0]++;
-      if (is_converter) {
-	stats[plural][1]++;
-      }
-    }
-    
   }
 
-  result << history_filter.size() << " users found\n";
+  for (int i = 0; i < crumb_stats.size(); i++) {
+    file << "------------------- " << i << " -------------------\n";
+    for (auto it = crumb_stats[i].begin(); it != crumb_stats[i].end(); it++) {
+      file << it->first << "\t" << it->second << "\n";
+    }
+  }
+ 
 
-  result << stats[0][0] << "," << stats[0][1] << "\n";
-  result << stats[1][0] << "," << stats[1][1] << "\n";
-  result << stats[2][0] << "," << stats[2][1] << "\n";
-  result << stats[3][0] << "," << stats[3][1] << "\n";
-  */
-  
+  result << "ok\n";
   char * buffer = (char *)malloc(result.str().size() + 1);
   memcpy(buffer, result.str().c_str(), result.str().size());
   buffer[result.str().size()] = 0;
