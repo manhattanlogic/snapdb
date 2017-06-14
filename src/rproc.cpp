be8abd856d85074f0b54b04f369a528144c8c015
manhattanlogic <roman@manhattanlogic.com>
2017-06-14 16:03:33 -0400

    a

--- src/rproc.cpp
+++ src/rproc.cpp
@@ -18,84 +18,37 @@ std::unordered_set<std::string> crumbs;
 
 extern "C"
 char * query() {
-  std::ofstream file("sku_stats.csv");
+  history_filter.clear();
   
-  std::unordered_map<int, int> vector_stats;
   std::stringstream result;
   for (auto i = json_history.begin(); i != json_history.end(); i++) {
-    bool is_converter = false;
+    bool is_revjet = false;
     for (auto j = i->second->history.begin(); j != i->second->history.end(); j++) {
-      bool is_product = false;
       if (j->second.events == NULL) continue;
       for (auto e = j->second.events->begin(); e != j->second.events->end(); e++) {
 	if (!(e->ensighten.exists)) continue;
 	for (auto ii = e->ensighten.items.begin(); ii != e->ensighten.items.end(); ii++) {
-	  if ((e -> ensighten.pageType == "PRODUCT") || (ii -> tag == "productpage")) {
-	    is_product = true;
-	    break;
-	  }
-	}
-	if (is_product) {
-	  for (int q = 0; q < e->ensighten.crumbs.size(); q++) {	    
-	    if (q >= crumb_stats.size()) {
-	      std::unordered_map<std::string, long> map;
-	      crumb_stats.push_back(map);
+	  if ((e -> ensighten.camGroup == "PRODUCT")) {
+	    if ((e->ensighten.camSource == "DglBrand") || (e->ensighten.camSource == "Digital Brand") ||
+		  (e->ensighten.camSource == "RevJet Acq")) {
+	      is_revjet = true;
 	    }
-	    auto it = crumb_stats[q].find(e->ensighten.crumbs[q]);
-	    crumbs.insert(e->ensighten.crumbs[q]);
-	    if (it == crumb_stats[q].end()) {
-	      crumb_stats[q][e->ensighten.crumbs[q]] = 1;
-	    } else {
-	      it->second++;
+	    if ((e->ensighten.camGroup == "DglBrand") || (e->ensighten.camGroup == "Digital Brand") ||
+		  (e->ensighten.camGroup == "RevJet Acq")) {
+	      is_revjet = true;
 	    }
 	  }
 	}
       }
     }
-  }
-
-  
-  std::ofstream out_file("crumbs_w2v_source.txt");
-  
-
-  for (auto i = json_history.begin(); i != json_history.end(); i++) {
-    bool is_converter = false;
-    bool printed = false;
-    for (auto j = i->second->history.begin(); j != i->second->history.end(); j++) {
-      
-      if (j->second.events == NULL) continue;
-      for (auto e = j->second.events->begin(); e != j->second.events->end(); e++) {
-	if (!(e->ensighten.exists)) continue;
-	
-	for (int q = 0; q < e->ensighten.crumbs.size(); q++) {	    
-	  std::string crumb = e->ensighten.crumbs[q];
-	  if (crumbs.find(crumb) != crumbs.end()) {
-	    if (printed) out_file << " ";
-	    std::replace(crumb.begin(), crumb.end(), ' ', '_');
-	    std::replace(crumb.begin(), crumb.end(), '\t', '_');
-	    std::replace(crumb.begin(), crumb.end(), '\n', '_');
-	    out_file << crumb;
-	    printed = true;
-	  }
-	}
-	
-      }
-    }
-    if (printed) out_file << "\n";
-  }
-
-
-
-  
-
-
-  
-  for (int i = 0; i < crumb_stats.size(); i++) {
-    file << "------------------- " << i << " -------------------\n";
-    for (auto it = crumb_stats[i].begin(); it != crumb_stats[i].end(); it++) {
-      file << it->first << "\t" << it->second << "\n";
+    if (is_revjet) {
+      history_filter.insert(i->first);
     }
   }
+
+  
+
+
  
 
   result << "ok\n";
