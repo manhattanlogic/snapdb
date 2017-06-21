3d2993846a9cb3cf2a3b11c40c56ef4011f88729
manhattanlogic <roman@manhattanlogic.com>
2017-06-21 12:59:11 -0400

    a

--- src/impressions_trainsform_filtered.cpp
+++ src/impressions_trainsform_filtered.cpp
@@ -157,7 +157,9 @@ int main(int argc, char ** argv) {
 	std::string e = "";
 	for (int i = 0; i < d["events"].Size(); i++) {
 	  if (d["events"][i].HasMember("ua") && d["events"][i]["ua"].IsObject()) {
-	    e += d["events"][i]["ua"]["_os"].GetString();
+	    std::string os = d["events"][i]["ua"]["_os"].GetString();
+	    std::string device = d["events"][i]["ua"]["_device_type"].GetString();
+	    e += "[" + os + " " + device + "]\t";
 	  } else {
 	    e += " - ";
 	  }
