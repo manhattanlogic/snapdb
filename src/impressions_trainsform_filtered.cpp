3dc0189f01981a18b79a80a514aa2d552031185b
manhattanlogic <roman@manhattanlogic.com>
2017-06-21 15:18:09 -0400

    a

--- src/impressions_trainsform_filtered.cpp
+++ src/impressions_trainsform_filtered.cpp
@@ -249,7 +249,10 @@ int main(int argc, char** argv) {
     auto parts = split_string(line, "\t");
     if (parts.size() < 6) continue;
     auto tags = split_string(parts[5], ",");
-    if (
+    if (tags.size() != 4) {
+      std::cerr << "tags.size():" << tags.size() << "\n";
+      continue;
+    }
     std::string record_id = parts[2] + "\t" + parts[3] + "\t" + parts[4] + "\t" + tags[2] + "\t" + tags[3];
     auto it = stats.find(record_id);
     if (it == stats.end()) {
